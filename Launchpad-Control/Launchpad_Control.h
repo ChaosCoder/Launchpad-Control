425a1a830ea66a9c94b8a89ced45bba2af6138a9
Andreas Ganske <ChaosCoder@andreass-imac.fritz.box>
2011-07-28 21:02:49 +0200

    Initial Commit

---
+++ Launchpad-Control/Launchpad_Control.h
@@ -0,0 +1,15 @@
+//
+//  Launchpad_Control.h
+//  Launchpad-Control
+//
+//  Created by Andreas Ganske on 28.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Launchpad_Control : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Launchpad-Control/Launchpad_Control.m
@@ -0,0 +1,17 @@
+//
+//  Launchpad_Control.m
+//  Launchpad-Control
+//
+//  Created by Andreas Ganske on 28.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Launchpad_Control.h"
+
+@implementation Launchpad_Control
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
